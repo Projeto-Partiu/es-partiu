dc1520d7ec8b0bead4a5a064560ee3bdb1571cf0
Lucas Cordeiro <ecdb.lucas@gmail.com>
2017-07-27 23:07:30 -0300

    adicionando fab para adicionar evento

--- android/Partiu/app/src/main/java/br/edu/ufcg/partiu/feed/FeedFragment.java
+++ android/Partiu/app/src/main/java/br/edu/ufcg/partiu/feed/FeedFragment.java
@@ -1,13 +1,17 @@
 package br.edu.ufcg.partiu.feed;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import br.edu.ufcg.partiu.R;
+import br.edu.ufcg.partiu.event.CreateEventActivity;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -18,12 +22,24 @@ public class FeedFragment extends Fragment implements FeedContract.View {
 
     private FeedContract.Presenter presenter;
 
+    @BindView(R.id.add_event_fab)
+    public FloatingActionButton fab;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_feed, container, false);
 
         ButterKnife.bind(this, view);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), CreateEventActivity.class);
+
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
