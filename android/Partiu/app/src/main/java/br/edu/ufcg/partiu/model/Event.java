48403b72339e5270e6fb585cdbae1f9b479bb390
ordansantos <citycold.ordan@gmail.com>
2017-07-30 02:32:37 -0300

    event view

---
+++ android/Partiu/app/src/main/java/br/edu/ufcg/partiu/model/Event.java
@@ -0,0 +1,8 @@
+package br.edu.ufcg.partiu.event;
+
+/**
+ * Created by ordan on 29/07/17.
+ */
+
+public class Event {
+}
---
+++ android/Partiu/app/src/main/java/br/edu/ufcg/partiu/service/EventServiceImpl.java
@@ -0,0 +1,8 @@
+package br.edu.ufcg.partiu.service;
+
+/**
+ * Created by ordan on 29/07/17.
+ */
+
+public class EventServiceImpl {
+}
